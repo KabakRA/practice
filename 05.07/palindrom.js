04f0ba0446705a526b1af7b08210d5fd86cf6d91
KabakRA <43096026+KabakRA@users.noreply.github.com>
2021-07-05 13:35:31 +0300

    Update palindrom.js

--- 05.07/palindrom.js
+++ 05.07/palindrom.js
@@ -1,16 +1,12 @@
-let str = 'k abak';
-
+let str = 'k abAk';
 function IsItPalindrom(str){
 	str.split(); //строка разбивается на массив символов
 	str1=[];
 	for(let i=0; i<str.length; i++){
-		if(str[i]!=' '){
-			
-			str1.push(str[i]);
-			
+		if(str[i]!=' '){			
+			str1.push(str[i].toLowerCase());			
 		}
-	}
-	
+	}	
 	for(let i=0, j=str1.length-1; i<=j; i++, j--){
 		if(str1[i]!=str1[j]){return('Not palindrom.')}
 	}
