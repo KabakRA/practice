f5acc35aeb7c83d7324f05e8cb7c453e421caeb0
KabakRA <43096026+KabakRA@users.noreply.github.com>
2021-07-05 15:58:44 +0300

    Update palindrom.js

--- 05.07/palindrom.js
+++ 05.07/palindrom.js
@@ -1,12 +1,14 @@
-let str = 'А роза упала на лапу Азора';
+let str = 'А-ро,за упала на л-апу Азора';
 function IsItPalindrom(str){
+	str = str.replace(/[^a-zа-яё]/gi, '');//убрать все, кроме букв
+	
 	str.split(); //строка разбивается на массив символов
-	str1=[];
-	for(let i=0; i<str.length; i++){
-		if(str[i]!=' '){			
+	let str1=[];
+	for(let i=0; i<str.length; i++){				
 			str1.push(str[i].toLowerCase());			
-		}
+		
 	}	
+	
 	for(let i=0, j=str1.length-1; i<=j; i++, j--){
 		if(str1[i]!=str1[j]){return('Not palindrom.')}
 	}
