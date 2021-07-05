9338ed02ae9b2b963b31eb0f00c710b339f3dd79
KabakRA <43096026+KabakRA@users.noreply.github.com>
2021-07-05 15:26:41 +0300

    Update filter_list.js

--- 05.07/filter_list.js
+++ 05.07/filter_list.js
@@ -2,12 +2,7 @@ let array = [1, 2, 3, 'a', 'f', 0, 'g', 8, 'e'];
 
 function filter_list(arr){
 	let filtered_arr=[];
-	
-	arr.forEach((item)=>{
-		if(typeof(item)=='number'){
-			filtered_arr.push(item);
-		}
-	})
+	filtered_arr = arr.filter(item =>typeof (item)=='number');	
 	return(filtered_arr);
 }
 
