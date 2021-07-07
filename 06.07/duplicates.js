4a18b708dad5bc0fe341b2589e3eed0fd5b04abf
KabakRA <43096026+KabakRA@users.noreply.github.com>
2021-07-07 10:58:58 +0300

    Update duplicates.js

--- 06.07/duplicates.js
+++ 06.07/duplicates.js
@@ -2,26 +2,20 @@ let str = 'heLlooOqQww122';
 function Duplicates(str){
 	let arr = [];
 	arr=str.split('');
-	let temp_arr = [];
-	temp_arr=str.split('');
 	let duplicates=0;	
 	for(let i=0; i<arr.length; i++){
-		arr[i]=arr[i].toLowerCase();
-		temp_arr[i]=temp_arr[i].toLowerCase();
+		arr[i]=arr[i].toLowerCase();	
 	}
-	for(let i=0; i<arr.length; i++){
-			if ((arr[i] != ')') && (arr[i] != '(')){ 
-				temp_arr[i]='used';	
-			if(temp_arr.includes(arr[i])){
-			duplicates++;	
-			for(let j=i+1; j<arr.length; j++){
-				if (arr[j]==arr[i]) {arr[j] = ')'} //символы-дубликаты преобразуем в ')'
-				
-			}
-			arr[i]=')';
-			}	else { arr[i]='('} //встречающиеся единожды преобразуем в '('
-			
-			}				
+	for(let i=0; i<arr.length-1; i++){
+			if(arr[i]!=')' && arr[i]!='('){
+			if(arr.indexOf(arr[i], i+1)!=-1){
+				duplicates++;
+while(arr.indexOf(arr[i], i+1)!=-1){
+	arr[arr.indexOf(arr[i], i+1)]=')';
+}			
+	arr[i]=')'
+			}else{arr[i]='('}
+	}
 	}
 			console.log('Всего встречено символов-дубликатов: '+duplicates);		
 			str=arr.join('');
