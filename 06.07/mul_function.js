0b4cedbe47a71fa7f50f6f8b74d1c8d9ab3d5d9a
KabakRA <43096026+KabakRA@users.noreply.github.com>
2021-07-07 11:33:26 +0300

    Update mul_function.js

--- 06.07/mul_function.js
+++ 06.07/mul_function.js
@@ -1,16 +1,9 @@
-const _mul = (x, y, z) => x * y * z;
-function curry(fn) {
-  const N = fn.length;
-  function innerFn(n, args) {
-    return function actualInnerFn(a) {
-      if(n <= 1) {
-        return fn(...args, a);
-      }
-     return innerFn(n - 1, [...args, a]);
-    }
-  }
-  return innerFn(N, [])
-}
-const mul = curry(_mul);
+const mul = (x) => {
+    return (y) => {
+        return (z) => {
+            return x * y * z;
+        };
+    };
+};
 console.log(mul(2)(3)(4)); 
 console.log(mul(4)(3)(4)); 
